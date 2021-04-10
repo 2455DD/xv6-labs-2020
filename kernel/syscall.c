7f02a91f46002b5a35a3621f69cead51b92a069c
2455dd <1026762637@qq.com>
2021-04-10 20:14:18 +0800

    First fix on systrace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -131,6 +131,32 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char *sysname[] = {
+[SYS_fork]    "fork",
+[SYS_exit]    "exit",
+[SYS_wait]    "wait",
+[SYS_pipe]    "pipe",
+[SYS_read]    "read",
+[SYS_kill]    "kill",
+[SYS_exec]    "exec",
+[SYS_fstat]   "stat",
+[SYS_chdir]   "chdir",
+[SYS_dup]     "dup",
+[SYS_getpid]  "getpid",
+[SYS_sbrk]    "sbrk",
+[SYS_sleep]   "sleep",
+[SYS_uptime]  "uptime",
+[SYS_open]    "open",
+[SYS_write]   "write",
+[SYS_mknod]   "mknod",
+[SYS_unlink]  "unlink",
+[SYS_link]    "link",
+[SYS_mkdir]   "mkdir",
+[SYS_close]   "close",
+[SYS_trace]   "trace",
+};
+
+
 void
 syscall(void)
 {
@@ -142,7 +168,7 @@ syscall(void)
     p->trapframe->a0 = syscalls[num]();       //a0存的是Func_ptr
     if(p->mask&&(1<<num)==1){                      //trace的条件: 当前进程的掩码存在且掩码中对应的控制bit为1
       printf("%d: %s read -> %d",
-            p->pid,p->name,p->trapframe->a0);
+            p->pid,sysname[num],p->trapframe->a0);
     }
   } else {
     printf("%d %s: unknown sys call %d\n",
